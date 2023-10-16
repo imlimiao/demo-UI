73e61313fbbac1615f20bd75c3d1f733d607a833
limiao04 <limiao04@58.com>
2023-10-16 17:15:09 +0800

    feat: 组件库初始化

---
+++ packages/index.js
@@ -0,0 +1,31 @@
+// 整个包的入口
+// 统一导出
+import Button from './Button/index'
+const components = [
+  Button,
+]
+// 定义install方法
+const install = function (Vue) {
+  // 注册所有的组件
+  components.forEach(component => {
+    Vue.component(component.name, component)
+  })
+}
+
+// 判断是否直接引入文件，如果是，就不用调用Vue.use()
+if (typeof window !== 'undefined' && window.Vue) {
+  install(window.Vue)
+}
+
+// 导出install方法
+export default {
+  install,
+  Button
+}
+
+// 支持按需导入
+export {
+  install,
+  Button
+}
+
